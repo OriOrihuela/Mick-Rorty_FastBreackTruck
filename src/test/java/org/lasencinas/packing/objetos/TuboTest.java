caa7a33d64c0bff72138fb9f8a4bb17ab12b4769
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-06-15 17:56:58 +0200

    TuboTest class added to remote.

---
+++ src/test/java/org/lasencinas/packing/objetos/TuboTest.java
@@ -0,0 +1,16 @@
+package org.lasencinas.packing.objetos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TuboTest {
+
+    @Test
+    public void envoltorio() {
+    }
+
+    @Test
+    public void toString1() {
+    }
+}
