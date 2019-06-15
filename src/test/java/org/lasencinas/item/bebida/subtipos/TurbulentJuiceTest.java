b73e5dade32d08fe2fa7e2e2226b47d98ac92e68
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-06-15 18:49:02 +0200

    All TurbulentJuiceTest cases passed :D.

--- src/test/java/org/lasencinas/item/bebida/subtipos/TurbulentJuiceTest.java
+++ src/test/java/org/lasencinas/item/bebida/subtipos/TurbulentJuiceTest.java
@@ -1,20 +1,41 @@
 package org.lasencinas.item.bebida.subtipos;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.lasencinas.item.bebida.Bebida;
+import org.lasencinas.packing.objetos.Tubo;
 
 import static org.junit.Assert.*;
 
 public class TurbulentJuiceTest {
 
-    @Test
-    public void nombre() {
+    /* ---- PROPERTIES ---- */
+    private Bebida turbulentJuice = null;
+
+
+    /* ---- BEFORE ---- */
+    @Before
+    public void setUp() {
+        turbulentJuice = new TurbulentJuice();
     }
 
     @Test
-    public void empaquetado() {
+    public void notNullTest() {
+        assertNotNull(turbulentJuice);
     }
 
     @Test
-    public void pvp() {
+    public void nombreTest() {
+        assertEquals("TurbulentJuice", turbulentJuice.nombre());
+    }
+
+    @Test
+    public void empaquetadoTest() {
+        assertEquals(Tubo.class, turbulentJuice.empaquetado().getClass());
+    }
+
+    @Test
+    public void pvpTest() {
+        assertEquals(30, turbulentJuice.pvp(), 0.01);
     }
 }
