259fd8f92a56415f8ad526cb3f4362db49e33227
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-06-15 19:01:12 +0200

    All TuboTest cases passed :D.

--- src/test/java/org/lasencinas/packing/objetos/TuboTest.java
+++ src/test/java/org/lasencinas/packing/objetos/TuboTest.java
@@ -1,16 +1,38 @@
 package org.lasencinas.packing.objetos;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.lasencinas.packing.Packing;
 
 import static org.junit.Assert.*;
 
 public class TuboTest {
 
-    @Test
-    public void envoltorio() {
+    /* ---- PROPERTIES ---- */
+    private Packing tubo = null;
+
+
+    /* ---- BEFORE ---- */
+    @Before
+    public void setUp() {
+        tubo = new Tubo("HakunaMatata");
     }
 
     @Test
-    public void toString1() {
+    public void notNullTest() {
+        assertNotNull(tubo);
+    }
+
+    @Test
+    public void envoltorioTest() {
+        assertEquals("HakunaMatata", tubo.envoltorio());
+    }
+
+    @Test
+    public void toStringTest() {
+        /**
+         * Must print "tubo"
+         */
+        System.out.println(tubo);
     }
 }
