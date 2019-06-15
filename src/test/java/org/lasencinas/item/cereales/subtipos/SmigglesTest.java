2be8823f3ea8fc85e11fc7b9113eed4c7972ad74
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-06-15 17:56:02 +0200

    SmigglesTest class added to remote.

---
+++ src/test/java/org/lasencinas/item/cereales/subtipos/SmigglesTest.java
@@ -0,0 +1,20 @@
+package org.lasencinas.item.cereales.subtipos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SmigglesTest {
+
+    @Test
+    public void nombre() {
+    }
+
+    @Test
+    public void empaquetado() {
+    }
+
+    @Test
+    public void pvp() {
+    }
+}
