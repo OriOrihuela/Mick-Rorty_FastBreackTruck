b47cff5e706ab13c4b1e294247c2cc09c2df49f9
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-06-15 17:53:31 +0200

    FastBreakTruckTest class added to remote.

---
+++ src/test/java/org/lasencinas/fastBreakTruck/FastBreakTruckTest.java
@@ -0,0 +1,20 @@
+package org.lasencinas.fastBreakTruck;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FastBreakTruckTest {
+
+    @Test
+    public void prepararEyeHoles() {
+    }
+
+    @Test
+    public void prepararSmiggles() {
+    }
+
+    @Test
+    public void incluirJuguete() {
+    }
+}
