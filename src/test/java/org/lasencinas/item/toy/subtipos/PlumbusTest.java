34e50e299011143eed5a5df187a7fc26c5af963d
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-06-15 18:54:29 +0200

    All PlumbusTest cases passed :D.

--- src/test/java/org/lasencinas/item/toy/subtipos/PlumbusTest.java
+++ src/test/java/org/lasencinas/item/toy/subtipos/PlumbusTest.java
@@ -1,20 +1,42 @@
 package org.lasencinas.item.toy.subtipos;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.lasencinas.item.toy.Toy;
+import org.lasencinas.packing.objetos.Caja;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class PlumbusTest {
 
-    @Test
-    public void nombre() {
+    /* ---- PROPERTIES ---- */
+    private Toy plumbus = null;
+
+
+    /* ---- BEFORE ---- */
+    @Before
+    public void setUp() {
+        plumbus = new Plumbus();
     }
 
     @Test
-    public void empaquetado() {
+    public void notNullTest() {
+        assertNotNull(plumbus);
     }
 
     @Test
-    public void pvp() {
+    public void nombreTest() {
+        assertEquals("Plumbus", plumbus.nombre());
+    }
+
+    @Test
+    public void empaquetadoTest() {
+        assertEquals(Caja.class, plumbus.empaquetado().getClass());
+    }
+
+    @Test
+    public void pvpTest() {
+        assertEquals(100, plumbus.pvp(), 0.01);
     }
 }
