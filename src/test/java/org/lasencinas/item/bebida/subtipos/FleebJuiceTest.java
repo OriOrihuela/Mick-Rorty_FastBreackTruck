13545a9f4340352dd9e624e3e55d0de0d806766d
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-06-15 17:53:57 +0200

    FleebJuiceTest class added to remote.

---
+++ src/test/java/org/lasencinas/item/bebida/subtipos/FleebJuiceTest.java
@@ -0,0 +1,20 @@
+package org.lasencinas.item.bebida.subtipos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FleebJuiceTest {
+
+    @Test
+    public void nombre() {
+    }
+
+    @Test
+    public void empaquetado() {
+    }
+
+    @Test
+    public void pvp() {
+    }
+}
