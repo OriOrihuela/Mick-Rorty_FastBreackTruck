74fa633a9b90add23df7bd9b90ff8e66a64d9f56
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-06-15 17:49:20 +0200

    CajaTest class added to remote.

---
+++ src/test/java/org/lasencinas/packing/objetos/CajaTest.java
@@ -0,0 +1,16 @@
+package org.lasencinas.packing.objetos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CajaTest {
+
+    @Test
+    public void envoltorio() {
+    }
+
+    @Test
+    public void toString1() {
+    }
+}
