7dd4e7969640de0f07acd8687d16679d5ce64e3b
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-06-15 17:51:03 +0200

    DesayunoTest class added to remote.

---
+++ src/test/java/org/lasencinas/desayuno/DesayunoTest.java
@@ -0,0 +1,33 @@
+package org.lasencinas.desayuno;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DesayunoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getItems() {
+    }
+
+    @Test
+    public void addCerealOrDrink() {
+    }
+
+    @Test
+    public void addToy() {
+    }
+
+    @Test
+    public void mostrarItems() {
+    }
+
+    @Test
+    public void getCoste() {
+    }
+}
