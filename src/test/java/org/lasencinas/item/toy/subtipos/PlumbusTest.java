cc55c9c05adea5b027a9fb92e64943f145c5243d
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-06-15 17:55:20 +0200

    PlumbusTest class added to remote.

---
+++ src/test/java/org/lasencinas/item/toy/subtipos/PlumbusTest.java
@@ -0,0 +1,20 @@
+package org.lasencinas.item.toy.subtipos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlumbusTest {
+
+    @Test
+    public void nombre() {
+    }
+
+    @Test
+    public void empaquetado() {
+    }
+
+    @Test
+    public void pvp() {
+    }
+}
