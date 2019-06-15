6b4fec2f643dd56879ed6cdb46fefa3264f5d9a4
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-06-15 18:58:37 +0200

    All CajaTest cases passed :D.

--- src/test/java/org/lasencinas/packing/objetos/CajaTest.java
+++ src/test/java/org/lasencinas/packing/objetos/CajaTest.java
@@ -1,16 +1,38 @@
 package org.lasencinas.packing.objetos;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.lasencinas.packing.Packing;
 
 import static org.junit.Assert.*;
 
 public class CajaTest {
 
-    @Test
-    public void envoltorio() {
+    /* ---- PROPERTIES ---- */
+    private Packing caja = null;
+
+
+    /* ---- BEFORE ---- */
+    @Before
+    public void setUp() {
+        caja = new Caja("HakunaMatata");
     }
 
     @Test
-    public void toString1() {
+    public void notNullTest() {
+        assertNotNull(caja);
+    }
+
+    @Test
+    public void envoltorioTest() {
+        assertEquals("HakunaMatata", caja.envoltorio());
+    }
+
+    @Test
+    public void toStringTest() {
+        /**
+         * Must print "caja"
+         */
+        System.out.println(caja);
     }
 }
