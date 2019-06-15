3dcc1dbb771674777f03ec0f44fe3a0871d9a68a
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-06-15 17:53:46 +0200

    FleebJuice class added to remote.

---
+++ src/main/java/org/lasencinas/item/bebida/subtipos/FleebJuice.java
@@ -0,0 +1,34 @@
+package org.lasencinas.item.bebida.subtipos;
+
+import org.lasencinas.item.bebida.Bebida;
+import org.lasencinas.packing.Packing;
+import org.lasencinas.packing.objetos.Tubo;
+
+public class FleebJuice extends Bebida {
+
+    /* ---- PROPERTIES ---- */
+    private Packing tubo = new Tubo("FleebJuice");
+    private float precio = 35;
+
+    /* ---- CONSTRUCTORS ---- */
+    public FleebJuice(){
+
+    }
+
+
+    /* ---- MAIN BEHAVIORUS ---- */
+    @Override
+    public String nombre() {
+        return tubo.envoltorio();
+    }
+
+    @Override
+    public Packing empaquetado() {
+        return tubo;
+    }
+
+    @Override
+    public Float pvp() {
+        return precio;
+    }
+}
