e53bb54e268edb29998d7a774c95235208012ea9
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-06-15 18:51:04 +0200

    All EyeholesTest cases passed :D.

--- src/test/java/org/lasencinas/item/cereales/subtipos/EyeholesTest.java
+++ src/test/java/org/lasencinas/item/cereales/subtipos/EyeholesTest.java
@@ -1,20 +1,42 @@
 package org.lasencinas.item.cereales.subtipos;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.lasencinas.item.cereales.Cereales;
+import org.lasencinas.packing.objetos.Caja;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class EyeholesTest {
 
-    @Test
-    public void nombre() {
+    /* ---- PROPERTIES ---- */
+    private Cereales eyeholes = null;
+
+
+    /* ---- BEFORE ---- */
+    @Before
+    public void setUp() {
+        eyeholes = new Eyeholes();
     }
 
     @Test
-    public void empaquetado() {
+    public void notNullTest() {
+        assertNotNull(eyeholes);
     }
 
     @Test
-    public void pvp() {
+    public void nombreTest() {
+        assertEquals("Eyeholes", eyeholes.nombre());
+    }
+
+    @Test
+    public void empaquetadoTest() {
+        assertEquals(Caja.class, eyeholes.empaquetado().getClass());
+    }
+
+    @Test
+    public void pvpTest() {
+        assertEquals(25, eyeholes.pvp(), 0.01);
     }
 }
